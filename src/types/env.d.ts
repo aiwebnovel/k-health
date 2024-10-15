0af240e9f244062ccefc75726fb2a7d22db857b1
rkdghwnd <ghwnd6448@gmail.com>
2024-10-15 17:19:54 +0900

    chore: env.d.ts

---
+++ src/types/env.d.ts
@@ -0,0 +1,12 @@
+interface ImportMeta {
+  env: {
+    VITE_FRONT_URL: string;
+    VITE_BACK_URL: string;
+    VITE_FIREBASE_API_KEY: string;
+    VITE_FIREBASE_AUTHDOMAIN: string;
+    VITE_FIREBASE_PROJECT_ID: string;
+    VITE_FIREBASE_STORAGE_BUCKET: string;
+    VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+    VITE_FIREBASE_APP_ID: string;
+  };
+}
