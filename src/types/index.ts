c04b9685f7d278beaf4be95cd1d5874cfb2d6100
rkdghwnd <ghwnd6448@gmail.com>
2024-10-15 17:20:16 +0900

    chore: types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,31 @@
+import { User } from '@firebase/auth';
+
 export type SelfTestResultTypes = {
   selfTestResultKey: string;
   updateSelfTestResultKey: (newKey: string) => void;
+};
+
+export type MyProfileResultTypes = {
+  myProfile: User | null;
+  updateMyProfile: (profile: User | null) => void;
+};
+
+export type LinkBoardTypes = {
+  id: number;
+  title: string;
+  image: string;
+  link: string;
+  linkPath: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type PostBoardTypes = {
+  id: number;
+  title: string;
+  content: string;
+  postPath: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
 };
